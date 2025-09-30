a3bbbe1694c8b49b431afde4f6f51807fb676ab2
ndkhair24-mu <22070920@vnu.edu.vn>
2025-09-30 10:58:58 +0700

    Create index.php

---
+++ week3/index.php
@@ -0,0 +1,12 @@
+<?php
+// Reverse string
+echo strrev(".drow olleH");
+echo "<br>";
+
+// Repeat string
+echo str_repeat("Hip ", 2);
+echo "<br>";
+
+// String to uppercase
+echo strtoupper("hooray!");
+?>
